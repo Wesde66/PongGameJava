b95b4371831ae977999518b9b05aa46d3c85a0eb
Wesde66@gmail.com <wesde@192.168.20.5>
2022-08-08 14:01:57 +1200

    Variables declared for window

--- The Game Pong/src/Pong.java
+++ The Game Pong/src/Pong.java
@@ -2,10 +2,14 @@ import javax.swing.JFrame;   //Adding the GUI
 
 public class Pong extends JFrame
 {
+	private final static String Window_Title = "Pong";
+	private final static int Window_Width = 800;
+	private final static int Window_Height = 600;
+	
 	public Pong()
 	{
-		setTitle("Pong");
-		setSize(800, 600);
+		setTitle(Window_Title);
+		setSize(Window_Width, Window_Height);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
