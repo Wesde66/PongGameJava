9042879a2c18b9a9fa10101e08df6751225fd8d1
Wesde66@gmail.com <wesde@192.168.20.5>
2022-08-09 12:09:04 +1200

    Dotted line created.

--- The Game Pong/src/PongPanel.java
+++ The Game Pong/src/PongPanel.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.BasicStroke;
+import java.awt.Stroke;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener
 {
@@ -29,8 +31,18 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
+
+	}
+	
+	public void paintDottedLine(Graphics g)
+	{
+		Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[ ]{9}, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth()/ 2, 0, getWidth()/ 2, getHeight());
+		g2d.dispose();
 	}
 
 	@Override
@@ -58,6 +70,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener
 	public void actionPerformed(ActionEvent event)
 	{
 		update();
+		repaint();
 		
 	}
 	
