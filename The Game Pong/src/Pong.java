b85943a61d4e2183feb214e50e025e1193a7e4e9
Wesde66@gmail.com <wesde@192.168.20.5>
2022-08-09 15:40:15 +1200

    Complete Game

--- The Game Pong/src/Pong.java
+++ The Game Pong/src/Pong.java
@@ -1,33 +1,30 @@
-import javax.swing.JFrame;   //Adding the GUI
+import javax.swing.JFrame;
 
-public class Pong extends JFrame
-{
-	private final static String Window_Title = "Pong";
-	private final static int Window_Width = 800;
-	private final static int Window_Height = 600;
+public class Pong extends JFrame {
 	
-	public Pong()
-	{
-		setTitle(Window_Title);
-		setSize(Window_Width, Window_Height);
-		setResizable(false);
-		add(new PongPanel());
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 600;
+	private final static int WINDOW_HEIGHT = 600;
+	
+	public Pong() {
+        setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setResizable(false);        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(new PongPanel());
+        setVisible(true);
+	}
+	
+	public static int getWindowWidth() {
+		return WINDOW_WIDTH;
+	}
+	
+	public static int getWindowHeight() {
+		return WINDOW_HEIGHT;
 	}
 
-	public static void main(String[] args) 
-	{
-		javax.swing.SwingUtilities.invokeLater(new Runnable()
-				{
-					public void run() 
-					{
-						new Pong();
-						
-					}
-				});
-		
-
+	public static void main(String[] args) {
+		new Pong();
 	}
 
 }
